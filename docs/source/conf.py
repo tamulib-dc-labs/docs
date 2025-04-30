c637f52c7c1ac26ffed284a0514ca467d651beb6
Mark Baggett <markpbaggett@gmail.com>
2025-04-30 12:15:11 -0500

    Init entities docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ author = 'Mark Baggett'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinxcontrib-mermaid']
 
 templates_path = ['_templates']
 exclude_patterns = []
